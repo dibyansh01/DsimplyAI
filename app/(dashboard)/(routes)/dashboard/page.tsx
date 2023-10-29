e0b7534af7cfd2c470195686ab68be05de58f88c
Dibyanshu <dibyansh01iitk@gmail.com>
2023-10-29 22:28:00 +0530

    clerk auth

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -4,8 +4,8 @@ import { Button } from '@/components/ui/button'
 export default function DashboardPage() {
   return (
     <div>
-      <p className="text-6xl">Hello world</p>
-      <Button variant="destructive">Click me</Button>
+      <p className="text-6xl">Dashboard Page (protected) </p>
+      
     </div>
     
   )
