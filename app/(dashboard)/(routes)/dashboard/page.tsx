6b310fe8ea374f2578b654240ec3fb132e4a3368
Dibyanshu <dibyansh01iitk@gmail.com>
2023-10-31 00:50:05 +0530

    sidebar

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,10 +1,13 @@
 import { Button } from '@/components/ui/button'
+import { UserButton } from '@clerk/nextjs'
 
 
 export default function DashboardPage() {
   return (
     <div>
+      
       <p className="text-6xl">Dashboard Page (protected) </p>
+      <UserButton afterSignOutUrl="/" />
       
     </div>
     
