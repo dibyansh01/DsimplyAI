fc3c4ff6b5348c7d0cd8f8617700ea50c78f3dab
Dibyanshu <dibyansh01iitk@gmail.com>
2023-11-02 00:25:30 +0530

    sidebar

---
+++ components/sidebar.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+const Sidebar = ()=> {
+    return (
+        <div className="space-y-4 py-4 flex flex-col h-full bg-[#234124] text-white">
+            Hello sidebar component
+
+        </div>
+    )
+}
+
+export default Sidebar;
