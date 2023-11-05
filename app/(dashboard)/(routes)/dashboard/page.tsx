343fc5029a80ebba3b3546b49d53090a9d75560c
Dibyanshu <dibyansh01iitk@gmail.com>
2023-11-05 15:19:46 +0530

    dashboard

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -3,7 +3,17 @@ export default function DashboardPage() {
   return (
     <div>
       
-      <p className="text-6xl">Dashboard Page (protected) </p>
+      <div className="mb-8 space-y-4">
+        <h2 className="text-2xl md:text-4xl font-bold text-center">
+          Unlock the potential of AI with our platform.
+        </h2>
+        <p className="text-muted-foreground font-light text-sm md:text-lg text-center">
+          Converse with the most advanced AI - Unleash the potential of artificial intelligence.
+        </p>
+      </div>
+      <div className="px-4 md:px-20 lg:px-32 space-y-4">
+
+      </div>
      
     </div>
     
