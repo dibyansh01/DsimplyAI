c00c2747d86b17d8ac4cd2b15adf3d89b98b4274
Dibyanshu <dibyansh01iitk@gmail.com>
2023-12-27 15:57:23 +0530

    fix:add isPro prop

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,16 +1,18 @@
 import Navbar from "@/components/navbar"
 import Sidebar from "@/components/sidebar"
 import { getAPILimitCount } from "@/lib/api-limit"
+import { checkSubscription } from "@/lib/subscription";
 
 const DashBoardLayout = async ({children} : {children: React.ReactNode}) => {
     
     const apiLimitCount = await getAPILimitCount();
+    const isPro = await checkSubscription();
 
     return (
         <div className="h-full relative">
             <div className="hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 bg-gray-700">
                 <div>
-                    <Sidebar apiLimitCount={apiLimitCount} />
+                    <Sidebar isPro={isPro} apiLimitCount={apiLimitCount} />
                 </div>
             </div>
             <main className="md:pl-72">
