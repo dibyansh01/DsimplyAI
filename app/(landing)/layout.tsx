bf201cb4a54d340ff659ee08ceed8be11e2d0f4d
Dibyanshu <dibyansh01iitk@gmail.com>
2024-01-05 23:49:29 +0530

    feat: landing page

---
+++ app/(landing)/layout.tsx
@@ -0,0 +1,14 @@
+
+const LandingLayout = ({children}: {children: React.ReactNode}) => {
+
+    return (
+        <main className="h-full bg-[#111827] ">
+            <div className="mx-auto max-w-screen-lg h-full w-full">
+                {children}
+            </div>
+
+        </main>
+    )
+}
+
+export default LandingLayout;
